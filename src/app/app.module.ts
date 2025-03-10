57a5a113c5a271e7f1898ac350086ecd21c8bd21
devmugo <mugomwash@gmail.com>
2025-03-10 15:05:59 +0300

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCustomerComponent } from './Customer/Components/addCustomer/add-customer/add-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCustomerComponent
   ],
   imports: [
     BrowserModule,
